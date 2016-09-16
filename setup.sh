4efdbd5e2d719b72432c0dfab5f8c99d6f4e1bb1
Milo Casagrande <milo.casagrande@linaro.org>
2016-09-16 15:15:59 +0200

    Rework gitolite setup process

--- setup.sh
+++ setup.sh
@@ -7,37 +7,49 @@ if [ -d ./.ssh ]; then
 fi
 
 # If we have a repositories/ directory make sure the owner is correct.
-if [ -d ./repositories ]; then
+if [ -d "./repositories" ]; then
     chown -R git:git ./repositories
 fi
 
-if [ ! -d ./.gitolite ]; then
-    # First run, need to pass the admin key.
-    if [ -n "$SSH_KEY" ]; then
-        echo "$SSH_KEY" > /tmp/admin.pub
+if [ -d "./gitolite-dir" ]; then
+    chown -R git:git "./gitolite-dir"
+fi
 
-        su git -c "/usr/local/bin/gitolite setup -pk /tmp/admin.pub"
+# First run: needs both admin key and the .gitolite dir doesn't exist.
+if [ -n "$SSH_KEY" -a ! -d "./repositories/gitolite-admin.git" ]; then
+    echo "$SSH_KEY" > /tmp/admin.pub
 
-        rm /tmp/admin.pub
-    else
-        # Move the bind mounted admin repo or gitolite will overwrite it.
-        if [ -d ./repositories/gitolite-admin.git ]; then
-            mv ./repositories/gitolite-admin.git /tmp/gitolite-admin-tmp.git
-        fi
+    su git -c "/usr/local/bin/gitolite setup -pk /tmp/admin.pub"
+
+    rm /tmp/admin.pub
+
+    # Move stuff into the bind mounted volute at ./gitolite-dir/.
+    cp -ax .gitolite/* ./gitolite-dir/
+    rm -rf .gitolite/
+    su git -c "ln -s ./gitolite-dir/ ./.gitolite"
+    chown -R git:git ./.gitolite
+else
+    if [ ! -h "./.gitolite" ]; then
+        rm -rf ./.gitolite/
+        su git -c "ln -s ./gitolite-dir/ ./.gitolite"
+        chown -R git:git ./.gitolite
+    fi
+
+    # Move the bind mounted admin repo or gitolite will overwrite it.
+    if [ -d "./repositories/gitolite-admin.git" ]; then
+        mv ./repositories/gitolite-admin.git /tmp/gitolite-admin-tmp.git
 
         su git -c "/usr/local/bin/gitolite setup -a dummy-admin"
 
         # Re-enable the old and correct admin repo.
-        if [ -d ./repositories/gitolite-admin.git ]; then
-            rm  -rf ./repositories/gitolite-admin.git
-            mv /tmp/gitolite-admin-tmp.git ./repositories/gitolite-admin.git
-        fi
+        rm  -rf ./repositories/gitolite-admin.git
+        mv /tmp/gitolite-admin-tmp.git ./repositories/gitolite-admin.git
 
         # Setup the correct authorized_keys based on gitolite-admin.
         su git -c "cd /home/git/repositories/gitolite-admin.git && GL_LIBDIR=$(/usr/local/bin/gitolite query-rc GL_LIBDIR) hooks/post-update refs/heads/master"
+
+        su git -c "/usr/local/bin/gitolite setup"
     fi
-else
-    su git -c "/usr/local/bin/gitolite setup"
 fi
 
 # Execute what remains.
